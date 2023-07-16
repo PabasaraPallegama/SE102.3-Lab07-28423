b3557390fca33fa5b8bb0d33993c2bbbf857a01d
PabasaraPallegama <136107658+PabasaraPallegama@users.noreply.github.com>
2023-07-16 14:04:50 +0530

    Question 1(php)-2023/07/14

---
+++ Q1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+$x=6;
+$y=4;
+$sum = $x+$y;
+$substraction = $x-$y;
+$multiplication = $x*$y;
+$division= $x/$y;
+echo"sum is :".$sum."<br>";
+echo"substraction is :".$substraction."<br>";
+echo"multiplication is :".$multiplication."<br>";
+echo"division is :".$division."<br>";
+?>
