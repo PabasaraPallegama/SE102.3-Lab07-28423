91ac0d3b7f155518a49a2f2f1ff01e0cadbd3d02
PabasaraPallegama <136107658+PabasaraPallegama@users.noreply.github.com>
2023-07-30 00:45:19 +0530

    Lab12-2023/07/28

---
+++ tuto final/createUser.php
@@ -0,0 +1,21 @@
+<?php$conn = mysqli_connect("localhost","root","","sample");
+if(!$conn)
+{
+    die("Connection failed:" . mysqli_connect_error());
+}
+$id = $_REQUEST['id'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$psw = $_REQUEST['password'];
+
+$sql="INSERT INTO STUDENTS(id,name,email,password)
+VALUES('" .$id . "','" .$name . "','" .$email . "','" .$psw . "')";
+
+if($conn->query($sql)===TRUE)
+{
+    echo "New User created successfully";
+}
+else{
+    echo"Error: " . $sql . "<br>" .$conn->error;
+}
+?>
